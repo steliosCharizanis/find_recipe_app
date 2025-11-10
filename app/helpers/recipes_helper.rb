8d9dcf026a4b85ba44ffbf652f4218778e4c55a1
scharizanis <stelios.charizanis@libramli.ai>
2025-11-10 21:57:39 +0200

    Remove unused RecipesHelper with fuzzy-string-match dependency

--- app/helpers/recipes_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'fuzzystringmatch'
-
-module RecipesHelper
-	def string_compare_metric(string_a, string_b)
-    jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
-    jarow.getDistance(string_a, string_b)
-  end
-end
